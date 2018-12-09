a22c22d003561ee8458004e4af48bda2a9a43169
JCsoftIA <ajcarlos032@gmail.com>
2018-12-09 09:51:47 -0600

    icon

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -10,7 +10,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         
         <!-- App favicon -->
-        <link rel="shortcut icon" href="assets/images/favicon.ico">
+        <link rel="shortcut icon" href="assets/images/logo-sm.png">
         <!-- App css -->
         <link rel="stylesheet" href="css/plantilla.css">
         <script src="js/modernizr.js"></script>
